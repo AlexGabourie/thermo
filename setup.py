4b6ebbce47e7c6cf5678552c7428fdf470ee2e34
Alex Gabourie <agabourie47@gmail.com>
2018-03-23 17:45:41 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='thermo',
+      version='1.0',
+      description='MD thermal properties functions',
+      author='Alexander Gabourie',
+      author_email='gabourie@stanford.edu',
+     )
