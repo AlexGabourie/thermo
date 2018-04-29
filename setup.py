f6544d6f2e943a0a84236a93dc6d56516cc2ffb0
Alex Gabourie <agabourie47@gmail.com>
2018-04-29 15:55:09 -0700

    tried updating setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import setuptools
 
 setup(name='thermo',
       version='1.0.1',
       description='MD thermal properties functions',
       author='Alexander Gabourie',
       author_email='gabourie@stanford.edu',
-      packages=['thermo'],
-      install_requires=['matplotlib', 'pyfftw', 'scipy'],
+      packages=setuptools.find_packages(),
+      install_requires=['matplotlib', 'pyfftw', 'scipy', 'ase'],
      )
