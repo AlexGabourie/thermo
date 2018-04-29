09ea77890109b3c4b63d23fe843d43d2af7c85a6
Alex Gabourie <agabourie47@gmail.com>
2018-04-29 15:27:40 -0700

    more init stuff

--- thermo/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-# from autocorr import *
-# from extract_dt	import *
-# from get_GKTC import *
-# from get_heat_flux import *
-# from metalToSI_TC import *
-# from get_sim_dimensions import *
-# from gpumd.get_gpumd_tc import *
