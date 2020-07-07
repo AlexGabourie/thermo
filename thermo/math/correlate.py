d4283f935dd64fbabe4ec36f9531bc6e63d59b46
AlexG <agabourie47@gmail.com>
2020-07-06 21:56:51 -0700

    Fix correlate - conjugation now on correct term
    Based on the inputs layout, a conjugation of an intermediate variable was flipped to be 'g' instead of 'f'. Now fixed to be consistent with expressions

--- thermo/math/correlate.py
+++ thermo/math/correlate.py
@@ -2,9 +2,10 @@ import pyfftw
 import multiprocessing
 import numpy as np
 
+
 def autocorr(f, max_lag):
-    '''
-    Computes a fast autocorrelation function and returns up to max_lag.
+    """
+    Computes a fast autocorrelation function <f*f> and returns up to max_lag.
 
     Args:
         f (ndarray):
@@ -16,15 +17,15 @@ def autocorr(f, max_lag):
     Returns:
         ndarray: Autocorrelation vector
 
-    '''
+    """
     N = len(f)
     d = N - np.arange(N)
     # https://dsp.stackexchange.com/questions/741/why-should-i-zero-pad-a-signal-before-taking-the-fourier-transform
-    f = np.lib.pad(f, (0,N), 'constant', constant_values=(0,0))
+    f = np.pad(f, (0, N), 'constant', constant_values=(0, 0))
     fvi = np.zeros(2*N, dtype=type(f[0]))
     fwd = pyfftw.FFTW(f, fvi, flags=('FFTW_ESTIMATE',), threads=multiprocessing.cpu_count())
     fwd()
-    inv_arg = fvi*np.conjugate(fvi)
+    inv_arg = np.conjugate(fvi)*fvi
     acf = np.zeros_like(inv_arg)
     rev = pyfftw.FFTW(inv_arg, acf, direction='FFTW_BACKWARD',
                       flags=('FFTW_ESTIMATE', ), threads=multiprocessing.cpu_count())
@@ -32,9 +33,10 @@ def autocorr(f, max_lag):
     acf = acf[:N]/d
     return np.real(acf[:max_lag+1])
 
+
 def corr(f, g, max_lag):
-    '''
-    Computes fast correlation function and returns up to max_lag. Assumes f and g are same length.
+    """
+    Computes fast correlation function <f*g> and returns up to max_lag. Assumes f and g are same length.
 
     Args:
         f (ndarray):
@@ -49,14 +51,14 @@ def corr(f, g, max_lag):
     Returns:
         ndarray: Correlation vector
 
-    '''
+    """
     if not len(f) == len(g):
         raise ValueError('corr arguments must be the same length.')
 
     N = len(f)
     d = N - np.arange(N)
-    f = np.lib.pad(f, (0,N), 'constant', constant_values=(0,0))
-    g = np.lib.pad(g, (0,N), 'constant', constant_values=(0,0))
+    f = np.pad(f, (0, N), 'constant', constant_values=(0, 0))
+    g = np.pad(g, (0, N), 'constant', constant_values=(0, 0))
 
     fvi = np.zeros(2*N, dtype=type(f[0]))
     gvi = np.zeros(2*N, dtype=type(g[0]))
@@ -67,7 +69,7 @@ def corr(f, g, max_lag):
     fwd_g = pyfftw.FFTW(g, gvi, flags=('FFTW_ESTIMATE',), threads=multiprocessing.cpu_count())
     fwd_g()
 
-    inv_arg = fvi*np.conjugate(gvi)
+    inv_arg = np.conjugate(fvi)*gvi
     cf = np.zeros_like(inv_arg)
     rev = pyfftw.FFTW(inv_arg, cf, direction='FFTW_BACKWARD',
                       flags=('FFTW_ESTIMATE', ), threads=multiprocessing.cpu_count())
