92f44d3998587385d3af71c94aa65d9a81d7b0e9
AlexG <agabourie47@gmail.com>
2020-09-09 12:05:40 -0700

    Update create_kpoints defaults

--- thermo/gpumd/io.py
+++ thermo/gpumd/io.py
@@ -255,7 +255,7 @@ def import_trajectory(filename='movie.xyz', in_file=None, atom_types=None):
 #########################################
 
 
-def create_kpoints(atoms, special_points='G', npoints=100):
+def create_kpoints(atoms, special_points='G', npoints=1):
     """
      Creates the file "kpoints.in", which specifies the kpoints needed for src/phonon
 
